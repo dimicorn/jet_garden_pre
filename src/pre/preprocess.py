6455d65a3baf22cff32d1530d75b3d80da8bce7e
dimicorn <51916503+dimicorn@users.noreply.github.com>
2025-11-27 00:41:52 +0300

    test: saving to png works

--- src/pre/preprocess.py
+++ src/pre/preprocess.py
@@ -3,16 +3,16 @@ import cv2 as cv
 from matplotlib.colors import LogNorm
 
 
-def rms(data: np.ndarray, k: float = 0.1) -> float:
+def map_noise(data: np.ndarray, k: float = 0.1) -> float:
     b1, b2 = int(k * data.shape[0]), int(k * data.shape[1])
     b3, b4 = int((1 - k) * data.shape[0]), int((1 - k) * data.shape[1])
 
-    upper_left = np.mean(data[:b1, :b2].flatten() ** 2)
-    upper_right = np.mean(data[b3:, :b2].flatten() ** 2)
-    down_left = np.mean(data[:b1, b4:].flatten() ** 2)
-    down_right = np.mean(data[b3:, b4:].flatten() ** 2)
-    noise = np.mean([upper_left, upper_right, down_left, down_right])
-    return np.sqrt(noise)
+    upper_left = np.std(data[:b1, :b2])
+    upper_right = np.std(data[b3:, :b2])
+    down_left = np.std(data[:b1, b4:])
+    down_right = np.std(data[b3:, b4:])
+    noise = np.median([upper_left, upper_right, down_left, down_right])
+    return noise
 
 
 def preprocess(raw_image: np.ndarray, shape: tuple[int] = (128, 128)) -> np.ndarray:
@@ -25,7 +25,7 @@ def preprocess_lognorm(
     raw_image: np.ndarray, shape: tuple[int] = (128, 128)
 ) -> np.ndarray:
     im = preprocess(raw_image, shape=shape)
-    vmin = min(rms(im) * 3, im.max())
+    vmin = min(map_noise(im) * 3, im.max())
     lognorm = LogNorm(vmin=vmin, vmax=None, clip=True)
     im_lognorm = lognorm(im)
     im_lognorm = np.ma.getdata(im_lognorm)
